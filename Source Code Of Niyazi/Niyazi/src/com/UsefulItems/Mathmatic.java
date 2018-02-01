e23dd98c41ba3b5defdb522f67809110deaf512d
Bilgehan NAL <bilgehanl.03@gmail.com>
2018-02-01 23:53:39 +0300

    Indentation problem solved

--- Source Code Of Niyazi/Niyazi/src/com/UsefulItems/Mathmatic.java
+++ Source Code Of Niyazi/Niyazi/src/com/UsefulItems/Mathmatic.java
@@ -3,7 +3,7 @@ package com.UsefulItems;
 public class Mathmatic {
 	
 	public static double abs(double a) {
-        return (a <= 0.0F) ? 0.0F - a : a;
+        	return (a <= 0.0F) ? 0.0F - a : a;
     }
 
 }
